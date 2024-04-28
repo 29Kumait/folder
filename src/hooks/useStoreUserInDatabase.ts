2674a5b56d381c5032d4170719304f3dfebf8627
kumait mustafa <kumait@gmail.com>
2024-04-28 06:40:03 +0200

    Refactor useStoreUserInDatabase hook to remove try catch

--- src/hooks/useStoreUserInDatabase.ts
+++ src/hooks/useStoreUserInDatabase.ts
@@ -5,22 +5,18 @@ import { User } from 'firebase/auth';
 export const useStoreUserInDatabase = (db: Database) => {
     return async (user: User) => {
         const userRef = ref(db, 'users/' + user.uid);
-        try {
-            await set(userRef, {
-                displayName: user.displayName,
-                email: user.email,
-                photoURL: user.photoURL,
-                providerData: user.providerData.map(data => ({
-                    providerId: data.providerId,
-                    uid: data.uid,
-                    displayName: data.displayName,
-                    email: data.email,
-                    photoURL: data.photoURL
-                })),
-                lastLogin: new Date().toISOString()
-            });
-        } catch (error) {
-            throw error;
-        }
+        await set(userRef, {
+            displayName: user.displayName,
+            email: user.email,
+            photoURL: user.photoURL,
+            providerData: user.providerData.map(data => ({
+                providerId: data.providerId,
+                uid: data.uid,
+                displayName: data.displayName,
+                email: data.email,
+                photoURL: data.photoURL
+            })),
+            lastLogin: new Date().toISOString()
+        });
     };
 };
