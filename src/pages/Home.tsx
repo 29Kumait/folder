74c0d1221e66a5231654020b89846c51aea7f9d4
kumait mustafa <kumait@gmail.com>
2024-05-09 07:58:28 +0200

    feat: Add authentication components and lazy loading for images on the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import GithubSignIn from '../components/GithubSignIn';
+import React from "react";
+import GithubSignIn from "../components/Authentication/GithubSignIn.tsx";
+import EmailSignIn from "../components/Authentication/EmailSignIn.tsx";
+import GoogleSignIn from "../components/Authentication/GoogleSignIn.tsx";
+import FetchImage from "../components/Image/FetchImage.tsx";
 const Home: React.FC = () => {
     return (
-        <div>
-            <GithubSignIn />
-            <h1>Github</h1>
-        </div>
+        <>
+            <div>
+                <GithubSignIn />
+                <h1>Github</h1>
+                <FetchImage path="github-icon.png" />
+            </div>
+            <div>
+                <GoogleSignIn />
+                <FetchImage path="Google-icon.png" />
+            </div>
+            <EmailSignIn />
+        </>
     );
 };
-
 export default Home;
